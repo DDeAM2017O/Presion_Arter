bc167aec33c6e914b168de3ae943dc4e803007fb
Jebus17 <33994727+Jebus17@users.noreply.github.com>
2017-11-30 21:08:30 -0600

    Exito-Parcial DB
    Ya no necesita ayuda de archivos para el cambio de fechas =D

--- app/src/main/java/com/example/jebus_vladimir/presion_arterial/Captura.java
+++ app/src/main/java/com/example/jebus_vladimir/presion_arterial/Captura.java
@@ -32,52 +32,49 @@ public class Captura extends Activity {
     private EditText a, b;
     private Calendar c;
     private SimpleDateFormat sdf;
-    private int res,idPer;
+    private int idPer;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.captura);
+        setContentView(R.layout.activity_captura);
         Intent intent = getIntent();
         idPer = intent.getIntExtra("id",0);
+
         a = (EditText) findViewById(R.id.user);
         b = (EditText) findViewById(R.id.pass);
         sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
         int val;
-
-
+        String time;
+        Random r = new Random();
         DataBase sistema = new DataBase(this, "app", null, 1);
         SQLiteDatabase db = sistema.getWritableDatabase();
-        Random r = new Random();
-        String qu = "select * from lectura;";
+
+        String qu = "select * from lectura where autor="+idPer+" order by fecha;";
         Cursor fila = db.rawQuery(qu, null);
         if (!fila.moveToFirst()) {
             c = Calendar.getInstance();
         }
         else{
-            FileInputStream iS;
-            try {
-                iS = openFileInput("resta.txt");
-                Reader reader = new InputStreamReader(iS);
-                BufferedReader br = new BufferedReader( reader );
-                res = Integer.parseInt(br.readLine());
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+            time = fila.getString( fila.getColumnIndex("fecha") );
+            Toast.makeText(this, time , Toast.LENGTH_SHORT).show();
+            Date date;
             c = Calendar.getInstance();
-            c.add(Calendar.DAY_OF_MONTH, res);
-            res--;
+            try {
+                date = sdf.parse(time);
+                c.setTime( date );
+                c.add(Calendar.DAY_OF_MONTH, -1);
+            } catch (ParseException e) {
+                Log.d("Error",e.toString());
+                Toast.makeText(this, "PTM" , Toast.LENGTH_SHORT).show();
+            }
+
             val = r.nextInt(183 - 67) + 67;
             a.setText( Integer.toString(val) );
             val = r.nextInt(123 - 47) + 47;
             b.setText( Integer.toString(val) );
-            FileOutputStream oS;
-            try{
-                oS = openFileOutput("resta.txt", Context.MODE_PRIVATE);
-                oS.write(Integer.toString(res).getBytes());
-            }catch (Exception e){
-                e.printStackTrace();
-            }
         }
+        db.close();
     }
 
     public void cancelar(View v) {
@@ -90,18 +87,18 @@ public class Captura extends Activity {
         SQLiteDatabase db = sistema.getWritableDatabase();
         ContentValues inst = new ContentValues();
         String aa, bb ,ti ;
-
+        long mm;
         aa = a.getText().toString().trim();
         bb = b.getText().toString().trim();
         ti = sdf.format(c.getTime());
-
+        mm = c.getTimeInMillis();
         //Toast.makeText(getApplicationContext(), ti, Toast.LENGTH_LONG).show();
         inst.put("fecha",ti);
         inst.put("alta", aa );
         inst.put("baja", bb );
         inst.put("autor",idPer);
         long idT = db.insert("lectura", null, inst);
-        //Toast.makeText(getApplicationContext(), "Fila:  " + idT, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Fila:  " + idT, Toast.LENGTH_LONG).show();
 
         if( Integer.valueOf(aa)>=180 || Integer.valueOf(bb)>=120  ) {
             String qu = "select * from medico;";
@@ -121,6 +118,7 @@ public class Captura extends Activity {
                 }
             }
         }
+        finish();
 
     }
 }
