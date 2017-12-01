b2623bb6bfb28b56953495695ca4c52950ebf809
Jebus17 <33994727+Jebus17@users.noreply.github.com>
2017-12-01 00:56:31 -0600

    Permisos
    Permisos mensajes

--- app/src/main/java/com/example/jebus_vladimir/presion_arterial/MainActivity.java
+++ app/src/main/java/com/example/jebus_vladimir/presion_arterial/MainActivity.java
@@ -7,13 +7,43 @@ import android.os.Bundle;
 import android.view.View;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  implements ActivityCompat.OnRequestPermissionsResultCallback {
+    final int REQUEST_MULTIPLE = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         JodaTimeAndroid.init(this);
         setContentView(R.layout.activity_main);
+        final Activity parent = this;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if ( ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.SEND_SMS)
+                        != PackageManager.PERMISSION_GRANTED ) {
+                    ActivityCompat.requestPermissions(parent, new String[]{
+                            Manifest.permission.SEND_SMS},REQUEST_MULTIPLE);
+                }
+            }
+        }, 500);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case REQUEST_MULTIPLE:
+                if(grantResults.length>0) {
+                    boolean smsPerm = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (!smsPerm) {
+                        finish();
+                    }
+                }
+                else {
+                    finish();
+                }
+                break;
+        }
     }
 
     public void startLogin(View v) {
